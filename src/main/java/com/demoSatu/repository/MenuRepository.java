e47d1d07bb14691a561b07470749f7eb3371fbee
zakhiyah <azakhiyah@gmail.com>
2025-01-05 20:25:16 +0700

    change menuId data type to UUID

--- src/main/java/com/demoSatu/repository/MenuRepository.java
+++ src/main/java/com/demoSatu/repository/MenuRepository.java
@@ -8,6 +8,7 @@ import com.demoSatu.model.Menu;
 
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
     List<Menu>findMenuByPrice(int price);
-    Menu findMenuByMenuName(String menuName);
+    Menu findMenuByMenuName(String menuName); // For retrieving the menu by name
+    boolean existsByMenuName(String menuName); // For checking duplicate menuName
     
 }
