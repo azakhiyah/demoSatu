22a1f751d5930ed99359784cced9d6b6ebe89923
zakhiyah <azakhiyah@gmail.com>
2025-01-07 10:33:10 +0700

    change column name from menuId to id, and add data annotations from lombok

--- src/main/java/com/demoSatu/model/Menu.java
+++ src/main/java/com/demoSatu/model/Menu.java
@@ -1,7 +1,6 @@
 package com.demoSatu.model;
 
 import java.util.List;
-import java.util.UUID;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,12 +9,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
 
-@Entity
+@Entity @Data
 public class Menu {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID menuId;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
     
     @Column(nullable = false, unique = true)
     private String menuName;
@@ -28,40 +28,5 @@ public class Menu {
 
     @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL)
     private List<Order> order;
-
-
-    public UUID getMenuId() {
-        return this.menuId;
-    }
-
-    public void setMenuId(UUID menuId) {
-        this.menuId = menuId;
-    }
-
-    public String getMenuName() {
-        return this.menuName;
-    }
-
-    public void setMenuName(String menuName) {
-        this.menuName = menuName;
-    }
-
-    public String getMenuDescription() {
-        return this.menuDescription;
-    }
-
-    public void setMenuDescription(String menuDescription) {
-        this.menuDescription = menuDescription;
-    }
-
-    public int getPrice() {
-        return this.price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-    
-
 
 }
