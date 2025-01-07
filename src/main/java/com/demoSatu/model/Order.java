6d423f2c5239ba7d2e091ab1f6a08f003828ca37
zakhiyah <azakhiyah@gmail.com>
2025-01-07 10:35:31 +0700

    change column name from OrderId to id, and add data annotations from lombok

--- src/main/java/com/demoSatu/model/Order.java
+++ src/main/java/com/demoSatu/model/Order.java
@@ -8,45 +8,21 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
-@Entity
+@Entity @Data
 @Table(name="orders")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int orderId;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 
     @Column
     private String customerName;
 
     @ManyToOne
-    @JoinColumn(name="menuId",referencedColumnName = "menuId")
+    @JoinColumn(name="menuId",referencedColumnName = "id")
     private Menu menu;
-
-
-    public int getOrderId() {
-        return this.orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getCustomerName() {
-        return this.customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public Menu getMenu() {
-        return this.menu;
-    }
-
-    public void setMenu(Menu menu) {
-        this.menu = menu;
-    }
 
 
 }
