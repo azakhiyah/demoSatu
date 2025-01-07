cade8ba8e133153e5220991ba3847525529c5493
zakhiyah <azakhiyah@gmail.com>
2025-01-07 10:50:42 +0700

    change Assert for menuId from getMenuId().isInstanceOf(UUID.class) to getId().matches(uuid -> uuid.matches(^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}))

--- src/test/java/com/demoSatu/repository/MenuRepositoryTests.java
+++ src/test/java/com/demoSatu/repository/MenuRepositoryTests.java
@@ -1,13 +1,13 @@
 package com.demoSatu.repository;
 
 import java.util.Locale;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.demoSatu.dao.MenuDao;
 import com.demoSatu.model.Menu;
 import com.github.javafaker.Faker;
 
@@ -15,7 +15,7 @@ import com.github.javafaker.Faker;
 public class MenuRepositoryTests {
 
     @Autowired
-    private MenuRepository menuRepository;
+    private MenuDao menuRepository;
     @Test
     public void testSaveMenu() {
         Faker faker = new Faker(new Locale("id"));
@@ -36,7 +36,9 @@ public class MenuRepositoryTests {
 
         // Assert
         assertThat(savedMenu).isNotNull();
-        assertThat(savedMenu.getMenuId()).isInstanceOf(UUID.class);
+        //assertThat(savedMenu.getMenuId()).isInstanceOf(UUID.class);
+        assertThat(savedMenu.getId())
+        .matches(uuid -> uuid.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")); // UUID format check
         assertThat(savedMenu.getMenuName()).isEqualTo(menuName);
         assertThat(savedMenu.getMenuDescription()).isEqualTo(menuDescription);
         assertThat(savedMenu.getPrice()).isEqualTo(price);
