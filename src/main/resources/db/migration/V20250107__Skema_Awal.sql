5e86fb8d5fbc879553d2a112a75ef37624f04803
zakhiyah <azakhiyah@gmail.com>
2025-01-08 14:48:08 +0700

    add create tabel orders query

--- src/main/resources/db/migration/V20250107__Skema_Awal.sql
+++ src/main/resources/db/migration/V20250107__Skema_Awal.sql
@@ -4,3 +4,9 @@ CREATE TABLE menu (
     menu_description VARCHAR(255) NOT NULL,
     price int NOT NULL
 );
+
+CREATE TABLE orders (
+    id varchar(36),
+    customer_name VARCHAR(50) NOT NULL,
+    menu_id VARCHAR(255) NOT NULL
+);
