17a663068fa66b249c73c78cfd05624f3f837c3b
zakhiyah <azakhiyah@gmail.com>
2025-01-12 21:55:20 +0700

    add GetMapping method for show sign-up form , PostMapping for save data input result from sign-up form into database

--- src/main/java/com/demoSatu/controller/RegisterController.java
+++ src/main/java/com/demoSatu/controller/RegisterController.java
@@ -1,13 +1,52 @@
 package com.demoSatu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.demoSatu.dao.MemberDao;
+import com.demoSatu.dto.RegistrationForm;
+import com.demoSatu.model.Member;
 
 @Controller
 public class RegisterController {
+    private final MemberDao memberDao;
+
+    @Autowired
+    public RegisterController(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
     @GetMapping("/sign-up")
-    public String showRegisterPage() {
+    public String register(Model member) {
+        member.addAttribute("registrationForm", new RegistrationForm());
         return "sign-up"; // Mengembalikan nama template "register.html"
     }
-    
+
+    @PostMapping("/sign-up")
+    public ModelAndView registerMember(@ModelAttribute RegistrationForm form) {
+        Member member = new Member();
+        member.setFullName(form.getFullName());
+        member.setEmail(form.getEmail());
+        member.setNomor_handphone(form.getPassword());
+        member.setPassword(form.getPassword());
+        memberDao.save(member);
+        return new ModelAndView("redirect:/sign-up-success");
+
+    }
+
+    @GetMapping("/sign-up-success")
+    public String signUpSuccess() {
+        return "sign-up-success"; // Refers to sign-up-success.html
+    }
+
+    @PostMapping("/addmember")
+    public String processRegister() {
+        return "sign-up-success";
+    }
+
 }
