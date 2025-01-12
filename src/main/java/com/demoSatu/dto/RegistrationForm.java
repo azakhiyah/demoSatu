9ac4b7a57ff9deb034dd52de2490f413a3040784
zakhiyah <azakhiyah@gmail.com>
2025-01-12 21:51:07 +0700

    add RegistrationForm as data transfer object from Registration form into database

---
+++ src/main/java/com/demoSatu/dto/RegistrationForm.java
@@ -0,0 +1,44 @@
+package com.demoSatu.dto;
+
+public class RegistrationForm {
+    private String fullName;
+    private String email;
+    private String phone;
+    private String password;
+
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
