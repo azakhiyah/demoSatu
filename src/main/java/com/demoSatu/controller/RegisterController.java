32614f660cdd57a51a00b24afdc4bc30fdcb75a7
zakhiyah <azakhiyah@gmail.com>
2025-01-27 21:55:55 +0700

    add validation for user input

--- src/main/java/com/demoSatu/controller/RegisterController.java
+++ src/main/java/com/demoSatu/controller/RegisterController.java
@@ -1,47 +1,55 @@
 package com.demoSatu.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.ModelAndView;
 
 import com.demoSatu.dao.MemberDao;
-import com.demoSatu.dto.RegistrationForm;
+import com.demoSatu.dto.SignUpForm;
 import com.demoSatu.model.Member;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class RegisterController {
     private final MemberDao memberDao;
 
-    @Autowired
+    
     public RegisterController(MemberDao memberDao) {
         this.memberDao = memberDao;
     }
 
-    @GetMapping("/sign-up")
-    public String register(Model member) {
-        member.addAttribute("registrationForm", new RegistrationForm());
-        return "sign-up"; // Mengembalikan nama template "register.html"
+    @GetMapping("/signup")
+    public String register(Model model) {
+        model.addAttribute("signup", new SignUpForm());
+        return "signup"; // Mengembalikan nama template "signup.html"
     }
 
-    @PostMapping("/sign-up")
-    public ModelAndView registerMember(@ModelAttribute RegistrationForm form) {
-        Member member = new Member();
-        member.setFullName(form.getFullName());
-        member.setEmail(form.getEmail());
-        member.setNomor_handphone(form.getPassword());
-        member.setPassword(form.getPassword());
-        memberDao.save(member);
-        return new ModelAndView("redirect:/sign-up-success");
+    @PostMapping("/signup")
+    public String registerMember(@Valid @ModelAttribute ("signup") SignUpForm signupForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+
+        Member member2 = new Member();
+        member2.setFullName(signupForm.getFullName());
+        member2.setEmail(signupForm.getEmail());
+        member2.setPhone(signupForm.getPhone());
+        System.out.println("Phone: " + signupForm.getPhone());
+        member2.setPassword(signupForm.getPassword());
+        memberDao.save(member2);
+        return "redirect:/signup-success";
 
     }
 
-    @GetMapping("/sign-up-success")
+    @GetMapping("/signup-success")
     public String signUpSuccess() {
-        return "sign-up-success"; // Refers to sign-up-success.html
+        return "signup-success"; // Refers to sign-up-success.html
     }
 
     @PostMapping("/addmember")
